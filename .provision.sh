8cffb525a029ff225873e95c5fe1000232a86d7c
Alexander Rudenko <alexander.rudenko.work@gmail.com>
2015-12-01 12:30:02 +0200

    edited vagrant provision script; applied dos2unix to both files

--- .provision.sh
+++ .provision.sh
@@ -7,12 +7,15 @@ sudo locale-gen utf-8
 sudo timedatectl set-timezone Europe/Kiev
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install -y mc
-sudo apt-get install -y python-software-properties python g++ make
+sudo apt-get install -y \
+	mc \
+	dos2unix \
+	python-software-properties python g++ make
 sudo add-apt-repository -y ppa:chris-lea/node.js
 sudo apt-get update
-sudo apt-get install -y nodejs	#npm will be installled as well
-sudo apt-get install -y git
+sudo apt-get install -y \
+	nodejs	\
+	git
 sudo git config --system user.name "Alexander Rudenko"
 sudo git config --system user.email alexander.rudenko.work@gmail.com
 sudo git config --system core.editor vim
