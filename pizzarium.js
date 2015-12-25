6fd07f1497323c9b5d8b5014d08c15c73b408f4c
Alexander Rudenko <alexander.rudenko.work@gmail.com>
2015-12-25 10:49:03 +0200

    extended onMessage logic

--- pizzarium.js
+++ pizzarium.js
@@ -4,64 +4,84 @@ module.exports = {
 	onMessage: onMessage
 };
 
-var pizzas = [];
+var order = [];
 var total = 0;
 var valueAdded = 0;
 var discount = 10;
-/*
+//TODO: парсить пиццы и пиццы недели
+
+//temporary constructor
 function pizza (name, priceS, priceM, priceL) {
 	this.name = name;
 	this.priceS = priceS;
 	this.priceM = priceM;
 	this.priceL = priceL;
-	}
 }
 
 var hawaiian = new pizza('Гавайская', 60, 100, 165);
 var neapolitan = new pizza('Неаполитанская', 55, 95, 150);
 var bavarian = new pizza('Баварская', 70, 120, 195);
+var pizzas = [hawaiian, neapolitan, bavarian];
 
-*/
+function userOrder (user, orderedPizza, price) {
+	this.user = user;
+	this.pizza =h
+}
+
 
 function onMessage (message) {
 	if (message.text === '!заказ') {
-		return onStart(message);
+		return onStart();
 	} else if (_.startsWith(message.text, '!хочу')) {
 		return onPizza(message);
-		//TODO PM принято
 	} else if (message.text === '!статус') {
-		return onStatus(message);	//TODO
+		return onStatus();
 	} else if (message.text === '!всё' || message.text === '!все') {
-		return onEnd(message);
+		return onEnd();
 	//TODO: если написали в личку боту, рандом отвечать
 	}
 }
 
-function onStart (message) {
+function onStart() {
 	return 'Начинаю собирать заказы.\nЧтобы совершить заказ, напишите `!хочу баварскую S`.\nЧтобы показать текущий заказ, напишите `!статус`.\nЧтобы закончить заказ, напишите `!всё`.';
 }
 
 function onPizza (message) {
-	//TODO: парсить пиццу недели
-	return 'pepyaka';
+	//TODO: игнорировать первый и два последних символа в названии пиццы ('Гавайская' vs 'гавайскую')
+	//TODO: PM принято с указанием цены со скидкой; Отвечать, если не знает такой пиццы или размер
+
+	var splittedMessage = message.text.split(' ');
+
+	for (i = 0; i < pizzas.length; i++) {
+		if (splittedMessage[1] === pizzas[i].name) {
+			if (splittedMessage[2] = 'S' || 's') {
+				return 'Вы заказали *' + pizzas[i].name + '* стоимостью *' + pizzas[i].priceS + '* грн.'; //TODO
+			}
+			else if (splittedMessage[2] = 'M' || 'm') {
+				return 'Вы заказали *' + pizzas[i].name + '* стоимостью *' + pizzas[i].priceM + '* грн.'; //TODO
+			}
+			else if (splittedMessage[2] = 'L' || 'l') {
+				return 'Вы заказали *' + pizzas[i].name + '* стоимостью *' + pizzas[i].priceL + '* грн.'; //TODO
+			}
+		}
+	}
+
+	//TODO: учитывать обе скидки
 	total += valueAdded; //TODO
 }
 
-function onStatus(message) {
-	return 'вася гавайская S'; //TODO
+function onStatus() {
+	return 'вася заказал баварскую S, а петя гавайскую M'; //TODO
 }
 
-function onEnd (message) {
+function onEnd() {
 	return 'Учитываю скидку 10% на всё меню. 20% скидку на пиццу недели пока не умею.\n ПОДСЧЁТ';	//TODO
+	//TODO: обнулять total и valueAdded в конце
 }
 
 
-
-/* TODO:
- - 4+1: если 4 человека, то. если 5 человек, то. если 6, то.
- - если 2 пиццы, то выбираем напиток.
- - пополам если два человека заказывают
- getPizzasPrice
- getPizzaOfTheWeek
- db with pizzas
+/*TODO:
+- 4+1 пицца либо делится на всех либо на одного человека (если 4, 5, 6 человек..)
+- если 2 пиццы, то выбираем напиток.
+- метод доставать имя пользователя по его slackID
  */
